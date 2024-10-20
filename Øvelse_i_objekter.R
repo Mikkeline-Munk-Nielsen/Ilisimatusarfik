4f5b4e034d7d9a507b07ebeffdd04b4f8fc824c9
Mikkeline-Munk-Nielsen <mikkelinemunk@gmail.com>
2024-10-20 13:41:05 -0100

    Third slide deck (unfinished)
    Changes made to code chunks, regarding colour theme etc.

---
+++ Øvelse_i_objekter.R
@@ -0,0 +1,21 @@
+# Øvelse i objekter:
+
+# Her er mit navn
+navn <- "Mikkeline"
+
+# Her er min alder
+alder <- 27
+
+
+# Her indlæses firma-datasættet
+df_firma <- readRDS("firma_data.rds")
+
+View(df_firma)
+
+names(df_firma)
+
+
+
+
+
+
