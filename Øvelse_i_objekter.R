3e4ef98e5902ffe0e77e6df08fecf2a25209e098
Mikkeline-Munk-Nielsen <mikkelinemunk@gmail.com>
2024-10-21 18:07:51 -0100

    Added codebook file

--- Øvelse_i_objekter.R
+++ Øvelse_i_objekter.R
@@ -15,6 +15,29 @@ View(df_firma)
 names(df_firma)
 
 
+# Installér tidyverse
+install.packages("tidyverse")
+
+library(tidyverse)
+library(dplyr)
+
+
+# Lav en ny variabel, der måler omsaetning i 1.000 kr
+df_firma <- df_firma %>% mutate(omsaetning_1000 = omsaetning/1000)
+
+
+# Lav en dikotom variabel der tager værdien 1, hvis firmaerne har erklæret sig “tilfreds” eller “meget tilfreds” med deres omsætning, ellers skal den tage værdien 0
+df_firma <- df_firma %>%
+  mutate(tilfreds_binear = ifelse(tilfredshed %in% c("Tilfreds", "Meget tilfreds"), 1, 0))
+
+# Lav et nyt datasæt kun med virksomheder fra sundhedsindustrien
+df_sunhed <- df_firma %>% filter(industri=="Sundhed")
+
+
+# Lav et nyt datasæt kun med firmaer, der har erklæret sig “tilfreds” eller “meget tilfreds” med deres omsætning (dvs. firmaer med værdien 1 på den variabel I lavede ovenfor)
+df_sunhed <- df_firma %>% filter(tilfreds_binear==1)
+
+
 
 
 
