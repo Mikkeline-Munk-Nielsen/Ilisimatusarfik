e65d1707dd9a8faa49cb628c07654c066742130a
Mikkeline-Munk-Nielsen <mikkelinemunk@gmail.com>
2025-02-19 14:12:34 -0200

    Minor changes.

---
+++ Eksamen/eksamen_eø_F25/Eksamens_script_fil.R
@@ -0,0 +1,34 @@
+################################################################################
+# R-script
+# Eksamensopgave i Statistik for erhvervsøkonomi, februar 2025
+################################################################################
+
+
+# Navn: 
+################################################################################
+
+
+# Indlæst biblioteker med pacman:
+pacman::p_load(tidyverse, janitor, DescTools, ggplot2, dplyr, readxl, texreg, 
+               writexl, lmtest, estimatr, broom)
+
+
+# Indlæs datasæt
+
+
+
+# Opgave 1
+
+
+
+
+
+# Opgave 2
+
+
+
+
+# Opgave ...
+
+
+
